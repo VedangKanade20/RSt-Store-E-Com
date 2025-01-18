127ca09c2ff9cfa595056fc520db672a1d2c8506
VedangKanade <vedangk20@gmail.com>
2025-01-18 16:05:09 +0530

    Added a server js file to run it locally

---
+++ localServer.js
@@ -0,0 +1,42 @@
+import colors from 'colors';
+import dotenv from 'dotenv';
+import express from 'express';
+import path from 'path';
+
+import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
+import orderRoutes from './routes/orderRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(express.json()); // Request body parsing
+
+app.get('/', (req, res) => {
+	res.send('API is running');
+});
+
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/uploads', uploadRoutes);
+
+// Create a static folder
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	);
+});
